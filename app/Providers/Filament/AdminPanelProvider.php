a7935a90613b8b530a7dda4c7dad3f8861b3964e
Praditya Putra Irawan <rradit885@gmail.com>
2026-09-18 09:15:08 +0700

    chore(filament): register all resources in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,10 +42,10 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->navigationGroups([
+                'Portal & Mading',
                 'Visual Board 5R',
                 'Inventaris',
                 'Sumber Daya Manusia',
-                'Portal',
                 'Manajemen Akses',
             ])
             ->pages([
