35e79a1330e787be8d1b10bb5b5f8e157074a625
Praditya Putra Irawan <rradit885@gmail.com>
2026-09-18 09:15:07 +0700

    feat(filament): update DepartmentResource

--- app/Filament/Resources/Departments/DepartmentResource.php
+++ app/Filament/Resources/Departments/DepartmentResource.php
@@ -20,11 +20,11 @@ class DepartmentResource extends Resource
 
     protected static ?string $pluralModelLabel = 'Daftar Departemen';
 
-    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-users';
-
     protected static string|\UnitEnum|null $navigationGroup = 'Sumber Daya Manusia';
 
-    protected static ?int $navigationSort = 1;
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-user-group';
+
+    protected static ?int $navigationSort = 41;
 
     public static function form(Schema $schema): Schema
     {
